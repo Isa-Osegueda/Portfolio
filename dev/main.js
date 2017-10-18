2f2232a38e81e51501c68d7e07f80c03f27cc885
Pastora Isabel Osegueda Morales <posegueda@b7-10.local>
2017-10-18 09:44:13 -0600

    subiendo material

---
+++ dev/main.js
@@ -0,0 +1,8 @@
+var homeA = document.getElementById('homeA');
+var homeP = document.getElementById('homeP');
+
+console.log('jaja');
+
+homeA.addEventListener("mouseover", function(){
+    alert('jaja');
+}
